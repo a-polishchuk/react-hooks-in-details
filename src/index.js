de198c99ac07d773f5318fd3a6190d00af63cd54
Andrey Polishchuk <polishchuk91@gmail.com>
2020-11-20 18:39:32 +0200

    useState with lazy initialization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { Example } from './useState/useCounter';
-import { Example } from './useState/useLegacyState';
+// import { Example } from './useState/useLegacyState';
+import { Example } from './useState/lazyInit';
 
 ReactDOM.render(
   <React.StrictMode>
