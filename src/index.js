f568397949d2d8f83bd10115db20d9d6b152eb7a
Andrey Polishchuk <polishchuk91@gmail.com>
2020-11-21 12:52:11 +0200

    staleState example

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 // import { Example } from './useState/useCounter';
 // import { Example } from './useState/useLegacyState';
 // import { Example } from './useState/lazyInit';
-import { Example } from './useState/storingFunctions';
+// import { Example } from './useState/storingFunctions';
+import { Example } from './useState/staleState';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Example />
+    <Example delay={3000} />
   </React.StrictMode>,
   document.getElementById('root')
 );
