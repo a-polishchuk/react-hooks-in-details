1d0c5632ff4a0cee3930bd027ef83051260413ea
Andrey Polishchuk <polishchuk91@gmail.com>
2020-11-21 13:36:00 +0200

    useLocalStorageState

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Example } from './useState/useCounter';
-// import { Example } from './useState/useLegacyState';
-// import { Example } from './useState/lazyInit';
-// import { Example } from './useState/storingFunctions';
-import { Example } from './useState/staleState';
+import { Example } from './misc/useLocalStorageState';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Example delay={3000} />
+    <Example />
   </React.StrictMode>,
   document.getElementById('root')
 );
