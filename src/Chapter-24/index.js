14e8a75533efb2f964a30f94cef6f80a30d8cc53
Andrey Polishchuk <a.polishchuk@geniusee.com>
2021-04-04 21:37:19 +0300

    prepared chapter 24

---
+++ src/Chapter-24/index.js
@@ -0,0 +1,69 @@
+import { useEffect, useState, useRef } from 'react';
+
+function usePrevious(value) {
+  const refContainer = useRef(value);
+
+  useEffect(() => {
+    refContainer.current = value;
+  }, [value]);
+
+  return refContainer.current;
+}
+
+function useHistory(value) {
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    setHistory((array) => [...array, value]);
+  }, [value]);
+
+  return history;
+}
+
+const DIGITS = [
+  ['7', '8', '9'],
+  ['4', '5', '6'],
+  ['1', '2', '3'],
+  ['', '0', ''],
+];
+
+const style = {
+  width: 40,
+  height: 40,
+  margin: 5,
+};
+
+function Row({ array, onClick }) {
+  return (
+    <div>
+      {array.map((digit) => {
+        return digit ? (
+          <button style={style} onClick={() => onClick(digit)}>
+            {digit}
+          </button>
+        ) : (
+          <button style={style}>&nbsp;</button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default function Chapter24() {
+  const [currentDigit, setCurrentDigit] = useState(0);
+  // const previousDigit = usePrevious(currentDigit);
+  const history = useHistory(currentDigit);
+
+  return (
+    <>
+      <h2>Chapter 24: usePrevious</h2>
+      {/* <h3>
+        Current digit: {previousDigit} {'>>'} {currentDigit}
+      </h3> */}
+      <h3>History: {history.join(' > ')}</h3>
+      {DIGITS.map((row) => (
+        <Row array={row} onClick={setCurrentDigit} />
+      ))}
+    </>
+  );
+}
