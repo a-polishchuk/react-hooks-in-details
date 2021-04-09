3635dab7b9e5ecd9f3ec437b7834910ba0399378
Andrey Polishchuk <a.polishchuk@geniusee.com>
2021-04-09 17:59:49 +0300

    Fixed chapter 21.

--- src/Chapter-21/index.js
+++ src/Chapter-21/index.js
@@ -23,21 +23,25 @@ export default function Chapter21() {
   );
   const [age, setAge] = useLocalStorage('age', 25);
 
-  const setFirstName = (event) => {
+  const updateFirstName = (event) => {
     setUser((user) => ({
       ...user,
       firstName: event.target.value,
     }));
   };
 
-  const setLastName = (event) => {
+  const updateLastName = (event) => {
     setUser((user) => ({
       ...user,
       lastName: event.target.value,
     }));
   };
 
-  const handleAgeChange = (event) => {
+  const updatePosition = (event) => {
+    setPosition(event.target.value);
+  };
+
+  const updateAge = (event) => {
     setAge(parseInt(event.target.value));
   };
 
@@ -49,28 +53,28 @@ export default function Chapter21() {
         style={inputStyle}
         placeholder="First name"
         value={user.firstName}
-        onChange={setFirstName}
+        onChange={updateFirstName}
       />
       <input
         type="text"
         style={inputStyle}
         placeholder="Last name"
         value={user.lastName}
-        onChange={setLastName}
+        onChange={updateLastName}
       />
       <input
         type="text"
         style={inputStyle}
         placeholder="Position"
         value={position}
-        onChange={setPosition}
+        onChange={updatePosition}
       />
       <input
         type="number"
         style={inputStyle}
         placeholder="Age"
         value={age}
-        onChange={handleAgeChange}
+        onChange={updateAge}
       />
     </>
   );
