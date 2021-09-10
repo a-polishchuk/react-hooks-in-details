aac970283fbbcb5f6331a48be04e220a45b0016e
Andrey Polishchuk <polishchuk91@gmail.com>
2021-09-10 15:47:23 +0300

    SWR: Mock user data with custom fetcher.

---
+++ src/Bonus/SWR/fetchMockUserData.js
@@ -0,0 +1,31 @@
+const USER_MOCK = {
+  id: 2,
+  name: 'Ervin Howell',
+  username: 'Antonette',
+  email: 'Shanna@melissa.tv',
+  address: {
+    street: 'Victor Plains',
+    suite: 'Suite 879',
+    city: 'Wisokyburgh',
+    zipcode: '90566-7771',
+    geo: {
+      lat: '-43.9509',
+      lng: '-34.4618',
+    },
+  },
+  phone: '010-692-6593 x09125',
+  website: 'anastasia.net',
+  company: {
+    name: 'Deckow-Crist',
+    catchPhrase: 'Proactive didactic contingency',
+    bs: 'synergize scalable supply-chains',
+  },
+};
+
+const FAKE_NETWORK_DELAY = 500;
+
+export function fetchMockUserData() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(USER_MOCK), FAKE_NETWORK_DELAY);
+  });
+}
