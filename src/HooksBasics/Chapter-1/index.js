c719ae189d803c81722453858bd4c02252737c67
Andrii Polishchuk <polishchuk91@gmail.com>
2022-05-23 21:15:19 +0300

    Toolbar component. Improved first chapter

--- src/HooksBasics/Chapter-1/index.js
+++ src/HooksBasics/Chapter-1/index.js
@@ -1,29 +1,40 @@
 import { useState } from 'react';
+import Toolbar from 'components/Toolbar';
 import Button from 'components/Button';
+
+const DELAY = 2000;
 
 export default function Chapter1() {
   const [clicks, setClicks] = useState(0);
   const [showClicks, setShowClicks] = useState(true);
 
-  const onClick = () => {
-    setTimeout(() => {
-      setClicks((prevClicks) => prevClicks + 1);
-    }, 2000);
+  const incrementCounter = () => {
+    setClicks(clicks + 1);
   };
 
-  const toggleShowClicks = () => setShowClicks((prevValue) => !prevValue);
+  const incrementWithDelay = () => {
+    setTimeout(() => setClicks((prevClicks) => prevClicks + 1), DELAY);
+  };
+
+  const toggleShowClicks = () => {
+    setShowClicks((prevValue) => !prevValue);
+  };
 
   return (
     <div>
       <h2>Chapter 1. useState</h2>
 
-      <div style={{ display: 'flex', gap: 10 }}>
-        <Button
-          onClick={onClick}
-          text={showClicks ? `Click me! ${clicks}` : 'Click me!'}
-        />
+      {showClicks && (
+        <div style={{ fontSize: 64, textShadow: '3px 3px #eadff9' }}>
+          {clicks}
+        </div>
+      )}
+
+      <Toolbar>
+        <Button onClick={incrementCounter} text="Increment counter" />
+        <Button onClick={incrementWithDelay} text="Increment with delay" />
         <Button onClick={toggleShowClicks} text="Toggle show clicks" />
-      </div>
+      </Toolbar>
     </div>
   );
 }
---
+++ src/components/Toolbar/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+
+export default function Toolbar({ children }) {
+  return <div className="toolbar">{children}</div>;
+}
