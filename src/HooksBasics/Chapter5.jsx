8ecf7fe17dba33e4ccedaf10a445cbf1ad78d919
Andrii Polishchuk <polishchuk91@gmail.com>
2022-06-02 17:05:30 +0300

    Chapter 5 small refactoring

--- src/HooksBasics/Chapter5.jsx
+++ src/HooksBasics/Chapter5.jsx
@@ -7,7 +7,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const onChange = (setter) => {
+const buildOnChange = (setter) => {
   return (event) => {
     const { value } = event.target;
     setter(value ? parseFloat(value) : 0);
@@ -20,7 +20,7 @@ export function StoringFunctions() {
   const [action, setAction] = useState(() => add);
   const [sign, setSign] = useState('➕');
 
-  const applyAction = (fn, fnSign) => {
+  const buildClickHandler = (fn, fnSign) => {
     return () => {
       setAction(() => fn);
       setSign(fnSign);
@@ -32,16 +32,22 @@ export function StoringFunctions() {
       <h2>Chapter 5. Storing function in useState</h2>
 
       <Toolbar>
-        <Button onClick={applyAction(add, '➕')} text="➕ Add" />
-        <Button onClick={applyAction(subtract, '➖')} text="➖ Subtract" />
-        <Button onClick={applyAction(divide, '➗')} text="➗ Divide" />
-        <Button onClick={applyAction(multiply, '✖️')} text="✖️ Multiply" />
+        <Button onClick={buildClickHandler(add, '➕')} text="➕ Add" />
+        <Button
+          onClick={buildClickHandler(subtract, '➖')}
+          text="➖ Subtract"
+        />
+        <Button onClick={buildClickHandler(divide, '➗')} text="➗ Divide" />
+        <Button
+          onClick={buildClickHandler(multiply, '✖️')}
+          text="✖️ Multiply"
+        />
       </Toolbar>
 
       <Toolbar>
-        <input type="number" value={a} onChange={onChange(setA)} />
+        <input type="number" value={a} onChange={buildOnChange(setA)} />
         <div style={{ minWidth: 10, textAlign: 'center' }}>{sign}</div>
-        <input type="number" value={b} onChange={onChange(setB)} />
+        <input type="number" value={b} onChange={buildOnChange(setB)} />
         <span> = {action ? action(a, b) : ''}</span>
       </Toolbar>
     </>
